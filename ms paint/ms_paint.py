f958016b3de163a62abe694d81f817c489b568d0
skyark747 <bscs22064@itu.edu.pk>
2023-05-11 13:01:12 -0700

    add new things

--- ms paint/ms_paint.py
+++ ms paint/ms_paint.py
@@ -12,6 +12,9 @@ class Paint:
         self.canvas.pack()
 
         self.canvas.bind("<B1-Motion>",self.brush)
+        
+        self.canvas.bind("<ButtonRelease-1>",self.brushend)
+            
 
         self.prev_x,self.prev_y=None,None
 
@@ -23,10 +26,20 @@ class Paint:
         if self.prev_x==None or self.prev_x==None:
             self.prev_x,self.prev_y=event.x,event.y
             return
-        self.canvas.create_line(self.prev_x,self.prev_y,event.x,event.y,width=2,fill="pink")
+        self.canvas.create_line(self.prev_x,self.prev_y,event.x,event.y,width=2,capstyle='round',fill="black")
         self.prev_x,self.prev_y=event.x,event.y
-        
+     
+    def brushend(self,event):
+        self.prev_x,self.prev_y=None,None
 
+    def eraser(self,event):
+        if self.prev_x==None or self.prev_x==None:
+            self.prev_x,self.prev_y=event.x,event.y
+            return
+        self.canvas.create_line(self.prev_x,self.prev_y,event.x,event.y,width=3,fill="white")
+        self.prev_x,self.prev_y=event.x,event.y
 
+    def eraserend(self,event):
+         self.prev_x,self.prev_y=None,None
 
 Paint(1300,700,"white").play()
