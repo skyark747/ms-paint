c45752087906d287b1be9ae4d63ffab277081c38
skyark747 <bscs22064@itu.edu.pk>
2023-05-19 14:23:33 -0700

    colors

--- ms paint/ms_paint.py
+++ ms paint/ms_paint.py
@@ -1,4 +1,6 @@
 
+from asyncio.windows_events import NULL
+from cgitb import text
 from tkinter import *
 from tkinter import colorchooser
 
@@ -10,7 +12,7 @@ class Paint:
         self.screen.geometry(str(width)+'x'+str(height))
 
         #Selection area
-        self.buttonarea=Canvas(self.screen,width=width,height=100,highlightbackground="black",highlightthickness=1)
+        self.buttonarea=Canvas(self.screen,width=width,height=100,highlightbackground="black",highlightthickness=2)
         self.buttonarea.pack()
 
         #Canvas making
@@ -25,12 +27,72 @@ class Paint:
         self.clear.place(x=5,y=70)
 
         self.pict=PhotoImage(file=r"D:\\color.PNG")
-        self.pict=self.pict.subsample(15,10)
+        self.pict=self.pict.subsample(50,30)
         self.colorbutton=Button(self.buttonarea,image=self.pict,bg="white",command=self.obj_tools.changecolorbrush)
-        self.colorbutton.place(x=1000,y=20)
+        self.colorbutton.place(x=1050,y=20)
 
-        #self.color=Button(self.buttonarea,image=self.pict,text="color 2",bg="white",command=self.obj_tools.changecoloreraser)
-        #self.color.place(x=1100,y=20)
+        self.color=Button(self.buttonarea,text="color 2",image=self.pict,bg="white",command=self.obj_tools.changecoloreraser)
+        self.color.place(x=1150,y=20)
+
+        self.picred=PhotoImage(file=r"D:\\red.PNG")
+        self.picred=self.picred.subsample(130,90)
+     
+        self.cl0=Button(self.buttonarea,image=self.picred,bg="white",command=self.clearbutton)
+        self.cl0.place(x=840,y=20)
+
+        self.picdarkred=PhotoImage(file=r"D:\\darkred.PNG")
+        self.picdarkred=self.picdarkred.subsample(9,10)
+     
+        self.cl=Button(self.buttonarea,image=self.picdarkred,bg="white",command=self.clearbutton)
+        self.cl.place(x=840,y=50)
+
+        self.picorange=PhotoImage(file=r"D:\\orange.PNG")
+        self.picorange=self.picorange.subsample(15,10)
+     
+        self.cl1=Button(self.buttonarea,image=self.picorange,bg="white",command=self.clearbutton)
+        self.cl1.place(x=870,y=20)
+
+        self.picyellow=PhotoImage(file=r"D:\\yellow.PNG")
+        self.picyellow=self.picyellow.subsample(18,10)
+     
+        self.cl2=Button(self.buttonarea,image=self.picyellow,bg="white",command=self.clearbutton)
+        self.cl2.place(x=900,y=20)
+
+        self.picgreen=PhotoImage(file=r"D:\\green.PNG")
+        self.picgreen=self.picgreen.subsample(13,10)
+     
+        self.cl3=Button(self.buttonarea,image=self.picgreen,bg="white",command=self.clearbutton)
+        self.cl3.place(x=930,y=20)
+
+        self.picpurple=PhotoImage(file=r"D:\\purple.PNG")
+        self.picpurple=self.picpurple.subsample(16,10)
+
+        self.cl4=Button(self.buttonarea,image=self.picpurple,bg="white",command=self.clearbutton)
+        self.cl4.place(x=960,y=20)
+
+        self.picbrown=PhotoImage(file=r"D:\\brown.PNG")
+        self.picbrown=self.picbrown.subsample(15,10)
+
+        self.cl5=Button(self.buttonarea,image=self.picbrown,bg="white",command=self.clearbutton)
+        self.cl5.place(x=870,y=50)
+
+        self.picparrot=PhotoImage(file=r"D:\\parrot.PNG")
+        self.picparrot=self.picparrot.subsample(17,10)
+
+        self.cl6=Button(self.buttonarea,image=self.picparrot,bg="white",command=self.clearbutton)
+        self.cl6.place(x=900,y=50)
+        
+        self.piclightblue=PhotoImage(file=r"D:\\pink.PNG")
+        self.piclightblue=self.piclightblue.subsample(14,10)
+
+        self.cl7=Button(self.buttonarea,image=self.piclightblue,bg="white",command=self.clearbutton)
+        self.cl7.place(x=930,y=50)
+
+        self.picturqoise=PhotoImage(file=r"D:\\turqoise.PNG")
+        self.picturqoise=self.picturqoise.subsample(17,10)
+
+        self.cl8=Button(self.buttonarea,image=self.picturqoise,bg="white",command=self.clearbutton)
+        self.cl8.place(x=960,y=50)
 
 
         self.pic=PhotoImage(file=r"D:\\brush.PNG")
@@ -56,11 +118,12 @@ class Tools:
         self.canvas=Area
         self.buttonarea=Selection
         self.brushcolor="black"
+        self.prevcolor="black"
         self.erasercolor="white"
 
         #Stroke size
         self.stroke_size=IntVar()
-        self.options=[1,3,5,7]    
+        self.options=[1,3,5,7,9]    
         self.size_list=OptionMenu(self.buttonarea,self.stroke_size,*self.options)
         self.size_list.place(x=600,y=20)
 
@@ -106,7 +169,14 @@ class Tools:
     def changecolorbrush(self):
         selectcolor=colorchooser.askcolor()
         self.brushcolor=selectcolor[1]
+        if selectcolor[1]!=None:
+            self.remembercolor
+        if selectcolor[1]==None:
+            self.brushcolor=self.prevcolor
    
+    def remembercolor(self):
+        self.prevcolor=self.brushcolor
+
     def changecoloreraser(self):
         selectcolor=colorchooser.askcolor()
         self.erasercolor=selectcolor[1]
