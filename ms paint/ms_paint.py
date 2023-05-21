10e420a82f01d8d0e8ad990af28a2c68a8dd8f3c
skyark747 <bscs22064@itu.edu.pk>
2023-05-21 11:46:26 -0700

    new shapes

--- ms paint/ms_paint.py
+++ ms paint/ms_paint.py
@@ -18,6 +18,9 @@ class Paint:
         self.shapescanvas=Frame(self.buttonarea,width=180,height=99,bg="lightblue")
         self.shapescanvas.place(x=376,y=2)
 
+        self.colorcanvas=Frame(self.buttonarea,width=333,height=99,bg="mistyrose")
+        self.colorcanvas.place(x=800,y=2)
+
         #Canvas making
         self.canvas=Canvas(self.screen,width=width,height=height,bg=color)
         self.canvas.pack()
@@ -74,14 +77,26 @@ class Paint:
         self.pentagonshape=Button(self.buttonarea,image=self.pentagonpic,bg="lightblue",relief="groove",command=self.obj_shapes.ispentagonbuttonpressed)
         self.pentagonshape.place(x=412,y=40)
 
+        self.hexagonpic=PhotoImage(file=r"D:\\hexagon.PNG")
+        self.hexagonpic=self.hexagonpic.subsample(x=40,y=47)
+
+        self.hexagonshape=Button(self.buttonarea,image=self.hexagonpic,bg="lightblue",relief="groove",command=self.obj_shapes.ishexagonbuttonpressed)
+        self.hexagonshape.place(x=447,y=40)
+
+        self.starpic=PhotoImage(file=r"D:\\st.PNG")
+        self.starpic=self.starpic.subsample(x=42,y=44)
+
+        self.starshape=Button(self.buttonarea,image=self.starpic,bg="lightblue",relief="groove",command=self.obj_shapes.isstarbuttonpressed)
+        self.starshape.place(x=483,y=40)
+
 
         self.pict=PhotoImage(file=r"D:\\color.PNG")
         self.pict=self.pict.subsample(50,30)
-        self.colorbutton=Button(self.buttonarea,image=self.pict,bg="white",command=self.obj_tools.changecolorbrush)
-        self.colorbutton.place(x=1050,y=20)
+        self.colorbutton=Button(self.buttonarea,image=self.pict,bg="white",relief="groove",command=self.obj_tools.changecolorbrush)
+        self.colorbutton.place(x=1000,y=20)
 
-        self.color=Button(self.buttonarea,text="color 2",image=self.pict,bg="white",command=self.obj_tools.changecoloreraser)
-        self.color.place(x=1150,y=20)
+        self.color=Button(self.buttonarea,text="color 2",image=self.pict,bg="white",relief="groove",command=self.obj_tools.changecoloreraser)
+        self.color.place(x=1080,y=20)
 
         self.picred=PhotoImage(file=r"D:\\red.PNG")
         self.picred=self.picred.subsample(130,90)
@@ -258,6 +273,10 @@ class Shapes:
         self.lastid=None
         self.previd=None
         self.newid=None
+        self.id1=None
+        self.id2=None
+        self.id3=None
+        self.id4=None
 
     def straightline(self,event):
         if self.shapeid is not None:
@@ -433,10 +452,126 @@ class Shapes:
         self.canvas.bind("<B1-Motion>",self.pentagon)
         self.canvas.bind("<ButtonRelease-1>",self.shapeend)
 
+    def hexagon(self,event):
+        if self.shapeid is not None:
+            self.canvas.delete(self.shapeid)
+            self.canvas.delete(self.lastid)
+            self.canvas.delete(self.newid)
+            self.canvas.delete(self.previd)
+            self.canvas.delete(self.storeid)
+
+        if self.prev_x==None or self.prev_y==None:
+            self.prev_x,self.prev_y=event.x,event.y
+            return
+        radius=abs(self.prev_x-event.x)+abs(self.prev_y-event.y)
+
+        x1=self.prev_x
+        y1=self.prev_y
+
+        x2=self.prev_x-radius/2
+        y2=self.prev_y-radius/2
+
+        x3=self.prev_x+radius/2
+        y3=self.prev_y+radius/2
+
+        x4=x1-radius/2
+        y4=y1+radius
+       
+        x5=x1+radius/2
+        y5=y1+radius
+
+        y6=y1+1.4*radius
+
+        self.shapeid=self.canvas.create_polygon(x1,y1,x3,y3,x1,y1,x2,y3,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+   
+        self.newid=self.canvas.create_polygon(x4,y4,x2,y3,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+        
+        self.previd=self.canvas.create_polygon(x5,y5,x3,y3,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+
+        self.lastid=self.canvas.create_polygon(x5,y5,x1,y6,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+       
+        self.storeid=self.canvas.create_polygon(x4,y4,x1,y6,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+
+    def ishexagonbuttonpressed(self):
+        self.canvas["cursor"]="tcross"
+        self.canvas.unbind("<B1-Motion>")     
+        self.canvas.unbind("<ButtonRelease-1>")
+
+        self.canvas.bind("<B1-Motion>",self.hexagon)
+        self.canvas.bind("<ButtonRelease-1>",self.shapeend)
+
+    def star(self,event):
+        if self.shapeid is not None:
+            self.canvas.delete(self.shapeid)
+            self.canvas.delete(self.lastid)
+            self.canvas.delete(self.newid)
+            self.canvas.delete(self.previd)
+            self.canvas.delete(self.storeid)
+            self.canvas.delete(self.id1)
+            self.canvas.delete(self.id2)
+            self.canvas.delete(self.id3)
+                        
+            self.canvas.delete(self.id4)
+        if self.prev_x==None or self.prev_y==None:
+            self.prev_x,self.prev_y=event.x,event.y
+            return
+        radius=abs(self.prev_x-event.x)+abs(self.prev_y-event.y)
+
+        x1=self.prev_x
+        y1=self.prev_y
+
+        x2=self.prev_x-radius/3
+        y2=self.prev_y-radius/2
+
+        x3=self.prev_x+radius/3
+        y3=self.prev_y+radius/2
+
+        x4=x1-1.3*radius
+        y4=y1+radius
+       
+        x5=x1+1.3*radius
+        y5=y1+radius
+
+        x6=x2-radius/3
+        y6=y3+radius/2.3
+        
+        x7=x3+radius/3
+        y7=y1+1.5*radius
+        
+        y8=y1+1.2*radius
+        x8=x2-0.7*radius
+        x9=x3+0.7*radius
+
+        self.shapeid=self.canvas.create_polygon(x1,y1,x3,y3,x1,y1,x2,y3,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+   
+        self.newid=self.canvas.create_polygon(x2,y3,x4,y3,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+        
+        self.previd=self.canvas.create_polygon(x5,y3,x3,y3,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+
+        self.lastid=self.canvas.create_polygon(x5,y3,x7,y6,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+       
+        self.storeid=self.canvas.create_polygon(x4,y3,x6,y6,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+        
+        self.id1=self.canvas.create_polygon(x6,y6,x8,y7,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+
+        self.id2=self.canvas.create_polygon(x8,y7,x1,y8,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+
+        self.id3=self.canvas.create_polygon(x1,y8,x9,y7,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+
+        self.id4=self.canvas.create_polygon(x9,y7,x7,y6,outline=self.paintobjs.brushcolor.get(),width=self.paintobjs.stroke_size.get(),fill="")
+
+    def isstarbuttonpressed(self):
+        self.canvas["cursor"]="tcross"
+        self.canvas.unbind("<B1-Motion>")     
+        self.canvas.unbind("<ButtonRelease-1>")
+
+        self.canvas.bind("<B1-Motion>",self.star)
+        self.canvas.bind("<ButtonRelease-1>",self.shapeend)
+
     def shapeend(self,event):
         self.prev_x,self.prev_y=None,None
         self.shapeid=None
-        self.previd,self.newid,self.lastid=None,None,None
+        self.previd,self.newid,self.lastid,self.storeid=None,None,None,None
 
 
 Paint(1300,700,"white").play()
